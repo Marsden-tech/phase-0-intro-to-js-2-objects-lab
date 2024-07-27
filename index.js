d4dc22726b5677e124e37f6298f4d751d6195a9a
Marsden Mathews Odhiambo <marsdenlwero@gmail.com>
2024-07-27 09:54:45 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    "name": "Mark Owiti",
+    "streetAddress": "Moi avenue, Nairobi"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: deletedEmployee, ...newEmployee } = employee
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
